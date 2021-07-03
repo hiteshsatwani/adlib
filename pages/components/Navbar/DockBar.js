1f8fd75dcc3b12c879d480cacb0a05d7d37f486d
Hitesh <hiteshsatwani74@gmail.com>
2021-07-04 01:42:16 +0400

    Updated Manifest ✅

--- pages/components/Navbar/DockBar.js
+++ pages/components/Navbar/DockBar.js
@@ -2,10 +2,28 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"; // Import the 
 import { faHome, faHandSparkles, faMusic, faUser, faUserSlash, faBroom } from "@fortawesome/free-solid-svg-icons"; // import the icons you need
 import Link from 'next/link'
 import { signIn, signOut, useSession } from 'next-auth/client'
+import { useState, useEffect } from 'react'
 
 const Dock = () => {
 
     const [session, loading] = useSession()
+
+    const renderpfp = () => {
+
+        const [pfp, setpfp] = useState("https://upload.wikimedia.org/wikipedia/commons/thumb/2/21/Solid_black.svg/1024px-Solid_black.svg.png")
+
+        useEffect(() => {
+            if (!session.user.image == "") {
+                setpfp(session.user.image)
+            }
+        }, []);
+
+        return (
+            <div className="m-auto" >
+                <img src={pfp} className="rounded-full h-7 w-7" />
+            </div>
+        )
+    }
 
     return (
         <div className="h-screen-25 bg-purple-200 grid grid-cols-4 gap-4 text-white">
@@ -26,9 +44,7 @@ const Dock = () => {
             </Link>
             {session &&
                 <Link href="/account">
-                    <div className="m-auto" >
-                        <img src={session.user.image} className="rounded-full h-7 w-7" />
-                    </div>
+                    {renderpfp()}
                 </Link>
             }
             {!session &&
